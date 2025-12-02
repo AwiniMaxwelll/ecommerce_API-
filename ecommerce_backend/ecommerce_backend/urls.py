9781c137a8ce386d4ca49954c1776a7f6baf513d
MAXWELL AWINI <awinimaxwell428@gmail.com>
2025-12-02 08:56:13 +0000

    uncommented the order endpoints

--- ecommerce_backend/ecommerce_backend/urls.py
+++ ecommerce_backend/ecommerce_backend/urls.py
@@ -44,7 +44,7 @@ urlpatterns = [
     # API endpoints
     path('api/auth/', include('ecommerce_backend.apps.users.urls')),
     path('api/products/', include('ecommerce_backend.apps.products.urls')),
-    # path('api/orders/', include('ecommerce_backend.apps.orders.urls')),
+    path('api/orders/', include('ecommerce_backend.apps.orders.urls')),
     
     # API Documentation
     path('', RedirectView.as_view(url='/swagger/', permanent=False)),
